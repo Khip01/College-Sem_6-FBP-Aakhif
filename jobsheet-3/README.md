0dbf29b2f57e382eea7343e17797069ffcaf6ed3
Khip01 <aakhifathallah24@gmail.com>
2026-02-22 15:36:11 +0700

    add: jobsheet 3 - practicum step 1

---
+++ jobsheet-3/README.md
@@ -0,0 +1,7 @@
+# D. Langkah Praktikum
+
+## 1. Routing Dasar (Static Routing)
+
+Saya sudah menambahkan halaman about dengan cara menambahkan file `about.tsx` di folder `pages`
+
+![Halmaan about dengan file about.tsx](image.png)
---
+++ jobsheet-3/my-app-praktikum-3/src/pages/about.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function About() {
+  return (
+    <div>
+      <Head>
+        <title>Halaman About</title>
+      </Head>
+      <main>
+        <h1>ini halaman about aakhif</h1>
+      </main>
+    </div>
+  );
+}
