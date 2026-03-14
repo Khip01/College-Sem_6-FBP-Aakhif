ec8761ec04cebdf508305baef40533393616316c
Khip01 <aakhifathallah24@gmail.com>
2026-03-14 16:57:45 +0700

    add: jobsheet 11 - practicum step 1
    Bagian 1 – Membuat Dynamic Route

---
+++ jobsheet-11/README.md
@@ -0,0 +1,11 @@
+# C. Langkah Praktikum
+
+## Bagian 1 – Membuat Dynamic Route
+
+Saya menambahkan link pada masing masing item, sehingga item bisa ditekan,
+
+![menambahkan link pada item produk](image.png)
+
+![tampilan url pada saat item produk di hover](image-1.png)
+
+![tampilan detail produk pada saat salah satu item produk ditekan](image-2.png)
---
+++ jobsheet-11/my-app-praktikum-10/src/components/layouts/Appshell/index.tsx
@@ -0,0 +1,28 @@
+import { useRouter } from "next/router";
+import Navbar from "../navbar";
+import Footer from "@/components/utility/footer";
+
+const disableNavbar = ["/auth/login", "/auth/register", "/404"];
+
+type AppShellProps = {
+  children: React.ReactNode;
+};
+
+const AppShell = (props: AppShellProps) => {
+  const { children } = props;
+  const { pathname } = useRouter();
+
+  // console.log(useRouter());
+
+  return (
+    <div className="min-h-screen flex flex-col">
+      <main className="flex-grow">
+        {!disableNavbar.includes(pathname) && <Navbar />}
+        {children}
+      </main>
+      {!disableNavbar.includes(pathname) && <Footer />}
+    </div>
+  );
+};
+
+export default AppShell;
